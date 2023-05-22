c03e3c09f14ce1815b100a457447377dd806a326
Jeon <wjswnsrb93@naver.com>
2023-05-22 17:44:21 +0900

    FIX: 여행 컨트롤러 수정(rest, cors)

--- src/main/java/com/ssafy/attraction/controller/AttractionController.java
+++ src/main/java/com/ssafy/attraction/controller/AttractionController.java
@@ -20,9 +20,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.attraction.model.AttractionDto;
 import com.ssafy.attraction.model.MyTripDto;
@@ -30,7 +32,8 @@ import com.ssafy.attraction.model.service.AttractionServiceImpl;
 import com.ssafy.attraction.model.service.IAttractionService;
 import com.ssafy.user.model.UserDto;
 
-@Controller
+@RestController
+@CrossOrigin("*")
 @RequestMapping("/attraction")
 public class AttractionController {
 	private static final Logger logger = LoggerFactory.getLogger(AttractionController.class);
@@ -45,20 +48,6 @@ public class AttractionController {
         this.aservice = aservice;
     }
 
-    @GetMapping
-	public String init(Model model) {
-		logger.info("Welcome attraction! .");
-		List<Integer> list = new ArrayList<>();
-
-		// model.addAttribute("sidos", sservice.getSidos());
-		return "trips/search";
-	}
-
-	@GetMapping("/myTrip")
-	public String myTrip() {
-
-		return "trips/mytrip";
-	}
 
 	@GetMapping("/addMyTrip")
 	public ResponseEntity<?> addMyTrip(@RequestParam String contentId, HttpSession session) throws Exception {
@@ -80,45 +69,5 @@ public class AttractionController {
 		return new ResponseEntity<List<String>>(list, HttpStatus.OK);
 	}
 
-
-	@GetMapping("/hotplace")
-	public String hotplace() {
-		return "trips/hotplace";
-	}
-	
-
-
-
-
-
-
-	// @GetMapping("/search")
-	// public ResponseEntity<?> search(@RequestParam String areaCode, @RequestParam String contentTypeId, @RequestParam String sigunguCode, Model model) {
-
-	// 	int sido_code = Integer.parseInt(areaCode);
-	// 	int contentType = contentTypeId == null ? -1 : Integer.parseInt(contentTypeId);
-	// 	int gugunCode = Integer.parseInt(sigunguCode);
-		
-		
-	// 	List<AttractionDto> attractions = aservice.getAttractions(sido_code, contentType, gugunCode);
-	// 	return new ResponseEntity<List<AttractionDto>>(attractions, HttpStatus.OK);
-	// }
-
-	// @GetMapping("gugun")
-	// public ResponseEntity<?> gugun(@RequestParam String sidoCode) {
-	// 	String sido = sidoCode;
-	// 	List<GuGunDto> guguns = gservice.getGuGuns(Integer.parseInt(sido));
-		
-	// 	return new ResponseEntity<List<GuGunDto>>(guguns, HttpStatus.OK);
-	// }
-
-
-	
-	// 	} else if (action.equalsIgnoreCase("addMyTrip")) {
-	// 		int contentId = Integer.parseInt(request.getParameter("contentId"));
-	// 		UserDto userinfo = (UserDto) request.getSession().getAttribute("loginUser");
-	// 		System.out.println(userinfo);
-	// 		tservice.addAttraction(new MyTripDto(contentId, userinfo.getId()));
-	// 	}
 
 }
