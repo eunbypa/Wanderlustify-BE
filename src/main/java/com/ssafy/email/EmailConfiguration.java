176b0df5b37804143a2a86d757e6a34b8ce0c48d
CHOEUNBY <eunbypa@gmail.com>
2023-05-25 00:08:19 +0900

    FIX: 스프링에서 이메일 보내기 오류 수정

---
+++ src/main/java/com/ssafy/email/EmailConfiguration.java
@@ -0,0 +1,36 @@
+package com.ssafy.email;
+
+import java.util.Properties;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class EmailConfiguration {
+    @Bean
+    public JavaMailSender javaMailService() {
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+
+        javaMailSender.setHost("smtp.gmail.com");
+        javaMailSender.setUsername("wanderlustify9@gmail.com");
+        javaMailSender.setPassword("fdyyyqdcdzfqdonv");
+
+        javaMailSender.setPort(587);
+
+        javaMailSender.setJavaMailProperties(getMailProperties());
+
+        return javaMailSender;
+    }
+
+    private Properties getMailProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("mail.transport.protocol", "smtp");
+        properties.setProperty("mail.smtp.auth", "true");
+        properties.setProperty("mail.smtp.starttls.enable", "true");
+        properties.setProperty("mail.smtp.starttls.required", "true");
+        properties.setProperty("mail.debug", "true");
+        return properties;
+    }
+}
